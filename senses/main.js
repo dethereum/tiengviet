61ca1bf05b85514be435d14f5dc872b024faab6c
haiturtle <33427880+dethereum@users.noreply.github.com>
2022-10-09 17:03:57 -0400

    ADD: vntk processing

--- senses/main.js
+++ senses/main.js
@@ -1,30 +1,66 @@
-var vntk = require('vntk');
-var dictionary = vntk.dictionary();
+const fs = require('fs')
+
+const vntk = require('vntk');
 
 const deck = require("../deck.json");
+
+
+const dictionary = vntk.dictionary();
 const vocab = deck['children'][2]['notes']
+
+const blacklist = [
+    'thư giãn',
+    'chảnh'
+]
+
+const countPosFrequency = (my_list) => {
+    // Creating an empty dictionary
+    const pos_freq = my_list.reduce((acc, { pos }) => {
+        if (acc[pos]) {
+            return { ...acc, [pos]: acc[pos] + 1 }
+        } else {
+            return { ...acc, [pos]: 1 }
+        }
+    }, {})
+
+    console.log(pos_freq)
+
+    const pos_keys = Object.keys(pos_freq);
+    const hasUniqueMeanings = pos_keys.reduce((acc, cur) => {
+        if (!acc) return false;
+
+        return pos_freq[cur] > 1 ? false : true
+    }, true)
+
+    return hasUniqueMeanings
+}
+
+
 
 
 for (let index = 0; index < vocab.length; index++) {
     let note = vocab[index];
 
-    // don't look up proper nouns they all fail
-    if (note.fields[2] != "proper_noun") {
+    if (note.tags.includes("tiengviet::meta::processed_by_vntk")) {
+        continue;
+    }
+
+    if (!note.tags.includes("tiengviet::meta::processed")) {
+        throw new Error('needs to be preprocessed by python')
+    }
+
+    // don't look up proper nouns (they all fail) OR blacklisted words
+    if (note.fields[2] != "proper_noun" && !blacklist.includes(note.fields[0])) {
         const senses = dictionary.lookup(note.fields[0])
 
         if (senses) {
             if (senses.length == 1) {
-                //console.log(`${note.fields[0]} is monosemous`)
                 note.tags.push('tiengviet::meta::meaning::monosemous')
             }
 
             if (senses.length > 1) {
-                note.tags.push('tiengviet::meta::meaning::ambiguous')
-
-                console.log(`\nSenses for ${note.fields[0]} \n`)
-                senses.forEach((sense) => {
-                    console.log(sense)
-                })
+                const hasUniqueMeanings = countPosFrequency(senses)
+                note.tags.push(`tiengviet::meta::meaning::${hasUniqueMeanings ? 'uniq_and_ambigious' : 'ambigious'}`)
             }
 
             if (senses.length < 1) {
@@ -35,5 +71,18 @@ for (let index = 0; index < vocab.length; index++) {
             note.tags.push('tiengviet::meta::missed_by_vntk')
         }
     }
+
+    note.tags = [...new Set(note.tags.map((tag) => tag == 'tiengviet::meta::processed' ? 'tiengviet::meta::processed_by_vntk' : tag))]
+    vocab[index] = note;
 }
 
+deck['children'][2]['notes'] = vocab;
+const jsonString = JSON.stringify(deck, null, 4);
+
+fs.writeFile("../deck.json", jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    }
+})
