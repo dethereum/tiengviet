f0e9c609b66600f7b31a3fc1f3f3d1d4da18837e
haiturtle <33427880+dethereum@users.noreply.github.com>
2022-10-09 20:51:11 -0400

    IMPROVE: extract processing fxnality into seperate fxn

--- senses/main.js
+++ senses/main.js
@@ -5,10 +5,14 @@ const { nanoid } = require('nanoid');
 
 const deck = require("../deck.json");
 
+const DECK_MAP = {
+    'VOCAB': 2,
+    'COMPLEX_VOCAB': 0
+}
+
 const PHRASE_MODEL_UUID = "6f97455a-3ca6-11ed-b75d-e7ec0f58ddc6"
 
 const dictionary = vntk.dictionary();
-const vocab = deck['children'][2]['notes']
 
 // THESE VIET WORDS ARE DELETED IN THE DECK NOW
 const blacklist = [
@@ -56,51 +60,59 @@ const createPhraseNotesFromAmbiVocab = (note, senses) => {
     })
 }
 
-for (let index = 0; index < vocab.length; index++) {
-    let note = vocab[index];
+const getProcessedNotes = (notes) => {
+    for (let index = 0; index < notes.length; index++) {
+        let note = notes[index];
 
-    if (note.tags.includes("tiengviet::meta::processed_by_vntk")) {
-        continue;
-    }
-
-    if (!note.tags.includes("tiengviet::meta::processed")) {
-        throw new Error('needs to be preprocessed by python')
-    }
-
-    // don't look up proper nouns (they all fail) OR blacklisted words
-    if (note.fields[2] != "proper_noun" && !blacklist.includes(note.fields[0])) {
-        const senses = dictionary.lookup(note.fields[0])
-
-        if (senses) {
-            if (senses.length == 1) {
-                note.tags.push('tiengviet::meta::meaning::monosemous')
-            }
-
-            if (senses.length > 1) {
-                note.tags.push('tiengviet::meta::meaning::ambigious')
-                note.tags.push(`tiengviet::meta::by_word::${note.fields[0].replace(" ", "_")}`)
-                createPhraseNotesFromAmbiVocab(note, senses)
-            }
-
-            if (senses.length < 1) {
-                console.log(`WTF ${note.fields[0]}`)
-            }
-        } else {
-            console.log(`MISS FOR ${note.fields[0]}`)
-            note.tags.push('tiengviet::meta::missed_by_vntk')
+        if (note.tags.includes("tiengviet::meta::processed_by_vntk")) {
+            continue;
         }
+
+        if (!note.tags.includes("tiengviet::meta::processed")) {
+            throw new Error('needs to be preprocessed by python')
+        }
+
+        // don't look up proper nouns (they all fail) OR blacklisted words
+        if (note.fields[2] != "proper_noun" && !blacklist.includes(note.fields[0])) {
+            const senses = dictionary.lookup(note.fields[0])
+
+            if (senses) {
+                console.log(`HIT FOR ${note.fields[0]}`)
+                if (senses.length == 1) {
+                    note.tags.push('tiengviet::meta::meaning::monosemous')
+                }
+
+                if (senses.length > 1) {
+                    note.tags.push('tiengviet::meta::meaning::ambigious')
+                    note.tags.push(`tiengviet::meta::by_word::${note.fields[0].replace(" ", "_")}`)
+                    createPhraseNotesFromAmbiVocab(note, senses)
+                }
+
+                if (senses.length < 1) {
+                    console.log(`WTF ${note.fields[0]}`)
+                }
+            } else {
+                console.log(`MISS FOR ${note.fields[0]}`)
+                note.tags.push('tiengviet::meta::missed_by_vntk')
+            }
+        }
+
+        // if note is for proper noun tag it with one meaning
+        if (note.fields[2] == "proper_noun") {
+            note.tags.push('tiengviet::meta::meaning::monosemous')
+        }
+
+        note.tags = [...new Set(note.tags.map((tag) => tag == 'tiengviet::meta::processed' ? 'tiengviet::meta::processed_by_vntk' : tag))]
+        notes[index] = note;
     }
 
-    // if note is for proper noun tag it with one meaning
-    if (note.fields[2] == "proper_noun") {
-        note.tags.push('tiengviet::meta::meaning::monosemous')
-    }
-
-    note.tags = [...new Set(note.tags.map((tag) => tag == 'tiengviet::meta::processed' ? 'tiengviet::meta::processed_by_vntk' : tag))]
-    vocab[index] = note;
+    return notes;
 }
 
-deck['children'][2]['notes'] = vocab;
+// PROCESS DECKS OF INTEREST (vocab)
+deck['children'][DECK_MAP['VOCAB']]['notes'] = getProcessedNotes(deck['children'][DECK_MAP['VOCAB']]['notes'])
+
+// UPDATE DECK WITH NLP INFORMATION
 const jsonString = JSON.stringify(deck, null, 4);
 
 fs.writeFile("../deck.json", jsonString, err => {
